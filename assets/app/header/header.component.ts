b4e572e4a197bfa8d333e7bcb582b1bf361b1f83
jogit82 <jo.c.keller@gmail.com>
2017-10-12 14:51:35 -0700

    added some text about the input textarea

--- assets/app/header/header.component.ts
+++ assets/app/header/header.component.ts
@@ -4,9 +4,10 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     template: `
     <div class="container-fluid">
+    <div class="row"><h3><p class="text-center">Qwk.io</p></h3></div>
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
-                <ul class="nav nav-pills">
+                <ul class="nav nav-pills text-center">
                     <li routerLinkActive="active"><a [routerLink]="['/create']">Create</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['/about']">About</a></li>
                     <li routerLinkActive="active"><a [routerLink]="['/guidelines']">Guidelines</a></li>
